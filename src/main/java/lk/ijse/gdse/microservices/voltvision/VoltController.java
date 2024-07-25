74826cbd89b29276bfea916539556dc29c1f1f2c
MSI <chathurabhashitha01@gmail.com>
2024-07-25 22:59:09 +0530

    cors error handled

--- src/main/java/lk/ijse/gdse/microservices/voltvision/VoltController.java
+++ src/main/java/lk/ijse/gdse/microservices/voltvision/VoltController.java
@@ -1,15 +1,13 @@
 package lk.ijse.gdse.microservices.voltvision;
 
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/volt")
+@CrossOrigin
 public class VoltController {
 
     @PostMapping("/save")
